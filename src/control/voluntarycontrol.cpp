11f575981c74057dcb575e316f380668d2b9847f
Florian Richer <florian.richer@upmc.fr>
2019-03-13 17:39:38 +0100

    Fixed wrong pin values and movement conditions

--- src/control/voluntarycontrol.cpp
+++ src/control/voluntarycontrol.cpp
@@ -7,8 +7,8 @@
 VoluntaryControl::VoluntaryControl() : _osmer(OsmerElbow::instance())
 {
     _settings.beginGroup("VoluntaryControl");
-    _pin_up = _settings.value("pin_up", 23).toInt();
-    _pin_down = _settings.value("pin_down", 24).toInt();
+    _pin_up = _settings.value("pin_up", 24).toInt();
+    _pin_down = _settings.value("pin_down", 22).toInt();
     set_period(_settings.value("period",0.01).toDouble());
 
     _menu.set_title("Voluntary Control");
@@ -28,15 +28,22 @@ void VoluntaryControl::setup() {
 }
 
 void VoluntaryControl::loop(double, double) {
-    if(digitalRead(_pin_down)) {
+    static int prev_pin_up_value = 1, prev_pin_down_value = 1;
+    int pin_down_value = digitalRead(_pin_down);
+    int pin_up_value = digitalRead(_pin_up);
+
+    if(pin_down_value == 0 && prev_pin_down_value == 1) {
         _osmer.set_velocity(30);
     }
-    else if(digitalRead(_pin_up)) {
+    else if(pin_up_value == 0 && prev_pin_up_value == 1) {
         _osmer.set_velocity(-30);
     }
-    else {
-        _osmer.set_velocity(0);
+    else if ((pin_down_value == 1 && pin_up_value == 1) && (prev_pin_down_value == 0 || prev_pin_up_value == 0)) {
+        _osmer.move_to_angle(_osmer.angle());
     }
+
+    prev_pin_down_value = pin_down_value;
+    prev_pin_up_value = pin_up_value;
 }
 
 void VoluntaryControl::cleanup() {
