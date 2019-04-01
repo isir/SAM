cd0148bc8297bf90639cde5c28ab2fb3b430fb9a
Florian Richer <florian.richer@upmc.fr>
2019-04-01 18:22:42 +0200

    Revamped using QThreads

--- src/peripherals/myoband/myoband.cpp
+++ src/peripherals/myoband/myoband.cpp
@@ -1,115 +1,45 @@
 #include "myoband.h"
-#include "localfeatures.h"
-#include "unistd.h"
+#include <QMutexLocker>
 
-using namespace myolinux;
+Myoband::Myoband()
+    : _client(myolinux::Serial("/dev/myoband", 115200))
+{
+    _client.onEmg([this](myolinux::myo::EmgSample sample) {
+        QMutexLocker lock(&_mutex);
+        for (unsigned i = 0; i < _NB_EMGS; i++)
+            this->_emgs[i] = sample[i];
+    });
 
-/**
- * \brief Myoband::Myoband Constructor
- * \param serial the USB stick port
- */
-Myoband::Myoband(Serial serial) : Client(serial) {
-    _emgs = new int[_NB_EMGS];
-
-    _old_emgs = new int*[_NB_EMGS];
-    for(int i=0; i<_NB_EMGS; i++) {
-        _old_emgs[i] = new int[_WIN_SIZE];
-        for(int j=0; j<_WIN_SIZE; j++)
-            _old_emgs[i][j] = 0;
-    }
-
-    _mean_emgs = new float[_NB_EMGS];
-    _rms = new float[_NB_EMGS];
-    _imus = new float[_NB_IMUS];
+    _client.onImu([this](myolinux::myo::OrientationSample ori, myolinux::myo::AccelerometerSample acc, myolinux::myo::GyroscopeSample gyr) {
+        QMutexLocker lock(&_mutex);
+        for (unsigned i = 0; i < 4; i++)
+            this->_imus[i] = (float)ori[i] / myolinux::myo::OrientationScale;
+        for (unsigned i = 0; i < 3; i++)
+            this->_imus[4 + i] = (float)acc[i] / myolinux::myo::AccelerometerScale;
+        for (unsigned i = 0; i < 3; i++)
+            this->_imus[7 + i] = (float)gyr[i] / myolinux::myo::GyroscopeScale;
+    });
 }
 
-/**
- * \brief Myoband::~Myoband Destructor
- */
-Myoband::~Myoband() {
-    delete _emgs;
-    delete _mean_emgs;
-    delete _imus;
-    for(int i=0; i<_NB_EMGS ; i++)
-        delete _old_emgs[i];
-    delete _old_emgs;
-    delete _rms;
+Myoband::~Myoband()
+{
 }
 
-/**
- * \brief Myoband::startListen Launchs the listening thread.
- */
-void Myoband::startListen() {
-    _stop_thread = false;
-    this->StartInternalThread();
+bool Myoband::setup()
+{
+    qInfo("MYOBAND : Trying to connect... Try to plug in/unplug the USB port\n");
+    _client.connect();
+    _client.setSleepMode(myolinux::myo::SleepMode::NeverSleep);
+    _client.setMode(myolinux::myo::EmgMode::SendEmg, myolinux::myo::ImuMode::SendData, myolinux::myo::ClassifierMode::Disabled);
+    return true;
 }
 
-/**
- * \brief Myoband::stopListen Stops the listening thread.
- */
-void Myoband:: stopListen() {
-    _stop_thread = true;
+void Myoband::loop(double, double)
+{
+    _client.listen();
 }
 
-/**
- * \brief Myoband::InternalThreadEntry Is the listening thread.
- */
-void Myoband::InternalThreadEntry() {
-
-    using namespace std::placeholders;
-
-    onEmg(std::bind(&Myoband::_update_emgs, this, _1));
-    onImu(std::bind(&Myoband::_update_imus, this, _1, _2, _3));
-
-    while(!_stop_thread) {
-        this->listen();
-    }
-
-}
-
-/**
- * \brief Myoband::_update_emgs EMGs callback. Executed when receiving new emgs from our Myoband's friend.
- * \param sample
- */
-void Myoband::_update_emgs(EmgSample sample) {
-    // Update EMGs
-    for (unsigned i = 0; i < 8; i++)
-        _emgs[i] = sample[i];
-
-    // Update window
-    for(int i=0; i<_NB_EMGS ; i++) {
-        for(int j=_WIN_SIZE-1; j>0; j--) {
-            _old_emgs[i][j] = _old_emgs[i][j-1];
-        }
-        _old_emgs[i][0] = _emgs[i];
-    }
-
-    // Extract RMS
-    for(int i=0 ; i<_NB_EMGS ; i++) {
-        _rms[i] = (float) extractRMS(_old_emgs[i], _WIN_SIZE);
-    }
-}
-
-/**
- * \brief Myoband::_update_imus IMUs callback. Executed when receiving new IMUs data from Myoband.
- * \param ori
- * \param acc
- * \param gyr
- */
-void Myoband::_update_imus(OrientationSample ori, AccelerometerSample acc, GyroscopeSample gyr) {
-    for(unsigned i=0 ; i<4 ; i++)
-        _imus[i] = (float) ori[i] / OrientationScale;
-    for(unsigned i=0 ; i<3 ; i++)
-        _imus[4+i] = (float) acc[i] / AccelerometerScale;
-    for(unsigned i=0 ; i<3 ; i++)
-        _imus[7+i] = (float) gyr[i] / GyroscopeScale;
-}
-
-/**
- * \brief Myoband::connect Trys to connect the Myoband via Bluetooth.
- * You can help it find it by pluging in/unpluging a powered micro USB cable into the Myoband.
- */
-void Myoband::connect() {
-    printf("MYOBAND INFO : Trying to connect... Try to plug in/unplug the USB port\n");
-    return  Client::connect();
+void Myoband::cleanup()
+{
+    _client.disconnect();
 }
--- src/peripherals/myoband/myoband.h
+++ src/peripherals/myoband/myoband.h
@@ -1,81 +1,58 @@
 #ifndef MYOBAND_H
 #define MYOBAND_H
 
-#include <pthread.h>
-#include "MyThreadClass.h"
 #include "myoLinux/myoclient.h"
 #include "myoLinux/serial.h"
+#include <QMutex>
+#include <control/basiccontroller.h>
 
-using namespace myolinux::myo;
-
-/**
- * \brief The Myoband class creates an instance to listen to the Myoband values.\n
- * Use it as follow :
- * - connect() until connected()
- * - setSleepMode() and setMode()
- * - Finally startListen()
- * - stopListen() and disconnect() after use
- * \n
- * \warning - vibrate() is not yet integrated. Please implemente it before using it.
- * \warning - When adding it in project, please add LocalFeatures lib BEFORE Myoband lib
- *
- */
-class Myoband : public Client, public MyThreadClass
-{
+class Myoband : public BasicController {
 
 public:
-    Myoband(myolinux::Serial serial);
+    Myoband();
     ~Myoband();
-    void        connect();
-    void        startListen();
-    void        stopListen();
+
+    bool setup();
+    void loop(double dt, double time);
+    void cleanup();
+
+    bool connected() { return _client.connected(); }
+
     /**
      * @brief getEMGs
      * @return Pointer of 8 EMGs i nrange [-127 ; 128].
      */
-    int*        getEMGs(){return _emgs;}
-    /**
-     * @brief getRMSs
-     * @return Pointer of 8 RMSs in range [0 ; 128].
-     */
-    float*      getRMSs() {return _rms;}
+    int* getEMGs() { return _emgs; }
     /**
      * @brief getIMUs
      * @return Pointer of 10 IMUs values : [ 4 Quats ; 3 Accs ; 3 Gyros].
      */
-    float*      getIMUs(){return _imus;}
+    float* getIMUs() { return _imus; }
     /**
      * @brief getAccs
      * @return Pointer of 3 Accs in g.
      */
-    float*      getAccs(){return &_imus[4];}
+    float* getAccs() { return &_imus[4]; }
     /**
      * @brief getQuats
      * @return Pointer of 4 Quats in std units.
      */
-    float*      getQuats(){return _imus;}
+    float* getQuats() { return _imus; }
     /**
      * @brief getGyros
      * @return Pointer of 3 Gyros in deg/s (to check).
      */
-    float*      getGyros(){return &_imus[7];}
+    float* getGyros() { return &_imus[7]; }
 
 private:
-    void        InternalThreadEntry();
-    void        _update_imus(OrientationSample ori, AccelerometerSample acc, GyroscopeSample gyr);
-    void        _update_emgs(EmgSample sample);
-    bool        _stop_thread;
+    myolinux::myo::Client _client;
+    QMutex _mutex;
 
-    const int   _NB_EMGS = 8;
-    const int   _NB_IMUS = 10;
-    const int   _WIN_SIZE = 20;
-    int*        _emgs;
-    int**       _old_emgs;
-    float*      _mean_emgs;
-    float*      _rms;
-    float*      _imus;
+    static const int _NB_EMGS = 8;
+    static const int _NB_IMUS = 10;
 
-
+    int _emgs[_NB_EMGS];
+    float _imus[_NB_IMUS];
 };
 
 #endif // MYOBAND_H
