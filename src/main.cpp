29bb4eb84f0848088402f913d010360b5643f35c
Florian Richer <florian.richer@sorbonne-universite.fr>
2019-05-17 18:33:21 +0200

    Lock memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "utils/samanager.h"
 
+#include <sys/mman.h>
+
 int main(int argc, char* argv[])
 {
+    mlockall(MCL_CURRENT | MCL_FUTURE);
+
     QCoreApplication::setOrganizationName("ISIR");
     QCoreApplication::setOrganizationDomain("isir.upmc.fr");
     QCoreApplication::setApplicationName("SAM");
