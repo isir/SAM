961d19b7ad76651177773cea3a36457dd0a5bcec
Florian Richer <florian.richer@sorbonne-universite.fr>
2019-05-21 16:54:39 +0200

    Open the port after creation

--- src/peripherals/roboclaw/factory.cpp
+++ src/peripherals/roboclaw/factory.cpp
@@ -9,10 +9,13 @@ RC::Factory::Factory()
 std::shared_ptr<SerialPort> RC::Factory::get(QString port_name, unsigned int baudrate)
 {
     if (!_map.contains(port_name)) {
-        return _map.insert(port_name, std::make_shared<SerialPort>(port_name, baudrate)).value();
+        std::shared_ptr<SerialPort> sp = std::make_shared<SerialPort>(port_name, baudrate);
+        sp->open();
+        _map.insert(port_name, sp);
+        return sp;
     }
     if (_map.value(port_name)->baudrate() != baudrate) {
-        throw std::runtime_error("Requested port is already in use with a different baudrate.");
+        throw std::runtime_error("Requested port is already in use with a different baudrate (" + std::to_string(_map.value(port_name)->baudrate()) + " - requested " + std::to_string(baudrate) + ").");
         return nullptr;
     }
     return _map.value(port_name);
