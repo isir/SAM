482400d33ab2c7764ccfbc266a3a971dbdbd47b5
Florian Richer <florian.richer@sorbonne-universite.fr>
2019-07-25 15:46:52 +0200

    Allow dynamic memory allocation w/ PREEMPT_RT
    https://rt.wiki.kernel.org/index.php/Dynamic_memory_allocation_example

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include "utils/samanager.h"
 #include <QCoreApplication>
+#include <espeak/speak_lib.h>
+#include <malloc.h>
 #include <sys/mman.h>
 
-#include <espeak/speak_lib.h>
+#define MEMORY_POOL (100 * 1024 * 1024) // 100MB
 
 int main(int argc, char* argv[])
 {
+    // Now lock all current and future pages from preventing of being paged
     mlockall(MCL_CURRENT | MCL_FUTURE);
+    // Turn off malloc trimming.
+    mallopt(M_TRIM_THRESHOLD, -1);
+    // Turn off mmap usage.
+    mallopt(M_MMAP_MAX, 0);
+
+    void* buf = malloc(MEMORY_POOL);
+    free(buf);
 
     QCoreApplication::setOrganizationName("ISIR");
     QCoreApplication::setOrganizationDomain("isir.upmc.fr");
