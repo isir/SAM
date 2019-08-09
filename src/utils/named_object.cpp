96f15a8a6750bc1ffab9312551dbf022df36ff44
Florian Richer <florian.richer@sorbonne-universite.fr>
2019-08-09 15:46:22 +0200

    Force lowercase names

--- src/utils/named_object.cpp
+++ src/utils/named_object.cpp
@@ -1,8 +1,11 @@
 #include "named_object.h"
+#include <algorithm>
 
 NamedObject::NamedObject(std::string name, const NamedObject* parent)
     : _name(name)
 {
+    std::transform(_name.begin(), _name.end(), _name.begin(), [](unsigned char c) { return std::tolower(c); });
+
     if (parent) {
         _parents = parent->_parents;
         _parents.push_back(parent->_name);
