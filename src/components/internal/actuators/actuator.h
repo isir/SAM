11880ce6a71cca4c6039e978fe5b8a1abbbb87b5
Mathilde <mathilde.lestoille@gmail.com>
2019-09-19 12:40:52 +0200

    add virtual incs_per_deg

--- src/components/internal/actuators/actuator.h
+++ src/components/internal/actuators/actuator.h
@@ -18,6 +18,7 @@ public:
     void set_velocity(double deg_s);
     virtual void set_velocity_safe(double deg_s);
     virtual bool is_calibrated() { return _calibrated; }
+    virtual uint32_t r_incs_per_deg() { return _incs_per_deg; }
 
 protected:
     virtual void on_exit();
