a65c3a2edb8f42ab374573d2c783bec71f695823
Mathilde <mathilde.lestoille@gmail.com>
2019-11-04 17:00:31 +0100

    Adding controlIMU class

--- src/control/controlimu.cpp
+++ src/control/controlimu.cpp
@@ -1,6 +1,356 @@
 #include "controlimu.h"
+#include "utils/check_ptr.h"
+#include "utils/log/log.h"
+#include "wiringPi.h"
+#include <filesystem>
+#include <iostream>
 
-ControlIMU::ControlIMU()
+ControlIMU::ControlIMU(std::string name, std::string filename, std::shared_ptr<SAM::Components> robot)
+    : ThreadedLoop(name, 0.01)
+    , _robot(robot)
+    , _k("k", BaseParam::ReadWrite, this, 5)
+    , _lt("lt(cm)", BaseParam::ReadWrite, this, 40)
+    , _lsh("lsh(cm)", BaseParam::ReadWrite, this, 20)
+    , _pin_up(24)
+    , _pin_down(22)
+    , _lua("lua(cm)", BaseParam::ReadWrite, this, 30)
+    , _lfa("lfa(cm)", BaseParam::ReadWrite, this, 20)
+    , _lwrist("lwrist(cm)", BaseParam::ReadWrite, this, 10)
+    , _lambdaE("lambda elbow", BaseParam::ReadWrite, this, 0)
+    , _lambdaWF("lambda wrist flex", BaseParam::ReadWrite, this, 0)
+    , _lambdaWPS("lambda wrist PS", BaseParam::ReadWrite, this, 0)
+    , _thresholdE("threshold E", BaseParam::ReadWrite, this, 5)
+    , _thresholdWF("threshold WF", BaseParam::ReadWrite, this, 5)
+    , _thresholdWPS("threshold WPS", BaseParam::ReadWrite, this, 5)
+{
+    if (!check_ptr(_robot->joints.elbow_flexion, _robot->joints.wrist_pronation, _robot->sensors.white_imu, _robot->sensors.red_imu, _robot->sensors.yellow_imu)) {
+        throw std::runtime_error("General Formulation Control is missing components");
+    }
+
+    _menu->add_item("tare", "Tare IMUs", [this](std::string) { this->tare_IMU(); });
+    _menu->add_item("analog", "Show IMU Analog data", [this](std::string) { this->analog_IMU(); });
+    _menu->add_item("calib", "Calibration", [this](std::string) { this->calibrations(); });
+
+    _menu->add_item(_robot->joints.elbow_flexion->menu());
+    _menu->add_item(_robot->joints.wrist_pronation->menu());
+    if (_robot->joints.hand)
+        _menu->add_item(_robot->joints.hand->menu());
+
+    if (_robot->joints.wrist_flexion) {
+        _menu->add_item(_robot->joints.wrist_flexion->menu());
+
+        _nbDOF = 3; // set the number of dof of the prosthetic arm to adapt intermediate computations in control law
+        debug() << "nDOF: " << _nbDOF;
+    } else {
+        _nbDOF = 2;
+        debug() << "nDOF: " << _nbDOF;
+    }
+
+    pullUpDnControl(_pin_up, PUD_UP);
+    pullUpDnControl(_pin_down, PUD_UP);
+
+    _filename = filename;
+}
+
+ControlIMU::~ControlIMU()
+{
+    _robot->joints.elbow_flexion->forward(0);
+    _robot->joints.wrist_pronation->forward(0);
+    stop_and_join();
+}
+
+void ControlIMU::analog_IMU()
+{
+    double a[8];
+    _robot->sensors.white_imu->get_analog(a);
+}
+
+void ControlIMU::tare_IMU()
+{
+    if (_robot->sensors.white_imu)
+        _robot->sensors.white_imu->send_command_algorithm_init_then_tare();
+    if (_robot->sensors.red_imu)
+        _robot->sensors.red_imu->send_command_algorithm_init_then_tare();
+    if (_robot->sensors.yellow_imu)
+        _robot->sensors.yellow_imu->send_command_algorithm_init_then_tare();
+
+    debug("Wait ...");
+
+    usleep(6 * 1000000);
+    _robot->user_feedback.buzzer->makeNoise(Buzzer::TRIPLE_BUZZ);
+}
+
+void ControlIMU::calibrations()
+{
+    // HAND
+    if (_robot->joints.hand) {
+        _robot->joints.hand->take_ownership();
+        _robot->joints.hand->init_sequence();
+        //        _robot->joints.hand->move(14);
+    }
+    // ELBOW
+    if (_robot->joints.elbow_flexion->is_calibrated() == false) {
+        _robot->joints.elbow_flexion->calibrate();
+    }
+    if (_robot->joints.elbow_flexion->is_calibrated())
+        debug() << "Calibration elbow: ok \n";
+
+    // WRIST FLEXION
+    if (_robot->joints.wrist_flexion) {
+        if (_robot->joints.wrist_flexion->is_calibrated() == false) {
+            _robot->joints.wrist_flexion->calibrate();
+        }
+        if (_robot->joints.wrist_flexion->is_calibrated())
+            debug() << "Calibration wrist flexion: ok \n";
+    }
+
+    // WRIST PRONATION
+    if (_robot->joints.wrist_pronation->is_calibrated() == false) {
+        _robot->joints.wrist_pronation->calibrate();
+    }
+    if (_robot->joints.wrist_pronation->is_calibrated())
+        debug() << "Calibration wrist pronation: ok \n";
+
+    _robot->joints.elbow_flexion->move_to(-60, 20);
+}
+
+void ControlIMU::displayPin()
+{
+    int pin_down_value = digitalRead(_pin_down);
+    int pin_up_value = digitalRead(_pin_up);
+    debug() << "PinUp: " << pin_up_value;
+    debug() << "PinDown: " << pin_down_value;
+}
+
+bool ControlIMU::setup()
+{
+    // Check for calibration
+    //    if (_robot->joints.wrist_flexion) {
+    //        if ((_robot->joints.elbow_flexion->is_calibrated() == false) || (_robot->joints.wrist_flexion->is_calibrated() == false) || (_robot->joints.wrist_pronation->is_calibrated() == false)) {
+    //            debug() << "Actuators not calibrated";
+    //            return false;
+    //        }
+    //    } else {
+    //        if ((_robot->joints.elbow_flexion->is_calibrated() == false)) {
+    //            debug() << "Elbow not calibrated";
+    //            return false;
+    //        }
+    //    }
+
+    // OPEN AND NAME DATA FILE
+    std::string suffix;
+
+    int cnt = 0;
+    std::string extension(".txt");
+    do {
+        ++cnt;
+        suffix = "_" + std::to_string(cnt);
+    } while (std::filesystem::exists(_filename + suffix + extension));
+
+    _file = std::ofstream(_filename + suffix + extension);
+    if (!_file.good()) {
+        critical() << "Failed to open" << (_filename + suffix + extension);
+        return false;
+    }
+    _need_to_write_header = true;
+    _start_time = clock::now();
+
+    // INITIALIZATION
+    _cnt = 0;
+    for (int i = 0; i < nbLinks; i++) {
+        _theta[i] = 0.;
+    }
+    return true;
+}
+
+void ControlIMU::loop(double, clock::time_point time)
 {
 
+    double timeWithDelta = (time - _start_time).count();
+
+    double debugData[40];
+
+    /// WRITE FILE HEADERS
+    if (_need_to_write_header) {
+        _file << " time, pinUp, pinDown,";
+        _file << " qWhite.w, qWhite.x, qWhite.y, qWhite.z, qTronc.w, qTronc.x, qTronc.y, qTronc.z,";
+        _file << " qYellow.w, qYellow.x, qYellow.y, qYellow.z,";
+        _file << " to complete,";
+        _file << "\r\n";
+        _need_to_write_header = false;
+    }
+
+    /// ELBOW
+    double elbowEncoder = _robot->joints.elbow_flexion->read_encoder_position();
+    /// WRIST
+    double pronoSupEncoder = _robot->joints.wrist_pronation->read_encoder_position();
+    debug() << "pronosup encoder: " << pronoSupEncoder;
+    double wristFlexEncoder = 0.;
+
+    /// PROTO with wrist flexor
+    if (_robot->joints.wrist_flexion) {
+        // set thresholds
+        _threshold[0] = _thresholdWF * M_PI / 180;
+        _threshold[1] = _thresholdWPS * M_PI / 180;
+        _threshold[2] = _thresholdE * M_PI / 180;
+        // set gain
+        _lambda[0] = _lambdaWF;
+        _lambda[1] = _lambdaWPS;
+        _lambda[2] = _lambdaE;
+        // set arm lengths
+        _l[0] = _lwrist; //  from the pronosupination joint to the wrist flexion/ext joint
+        _l[1] = _lfa; //  from the wrist flex/ext joint to the elbow flex/ext joint
+        _l[2] = _lua; // from the elbow joint to the acromion/shoulder joint
+
+        wristFlexEncoder = _robot->joints.wrist_flexion->read_encoder_position();
+        _theta[0] = -wristFlexEncoder / _robot->joints.wrist_flexion->r_incs_per_deg();
+        _theta[1] = -pronoSupEncoder / _robot->joints.wrist_pronation->r_incs_per_deg();
+        _theta[2] = -elbowEncoder / _robot->joints.elbow_flexion->r_incs_per_deg();
+        if (_cnt % 50 == 0)
+            debug() << "_theta(deg): " << _theta[0] << ", " << _theta[1] << ", " << _theta[2] << "\r\n";
+        _theta[0] = -M_PI / 2 + _theta[0] * M_PI / 180;
+        _theta[1] = M_PI / 2 + _theta[1] * M_PI / 180;
+        _theta[2] = M_PI / 2 + _theta[2] * M_PI / 180;
+
+    }
+    /// PROTO without wrist flexor
+    else {
+        // set thresholds
+        _threshold[0] = _thresholdWPS * M_PI / 180;
+        _threshold[1] = _thresholdE * M_PI / 180;
+
+        // set gain
+        _lambda[0] = _lambdaWPS;
+        _lambda[1] = _lambdaE;
+
+        // set arm lengths
+        _l[0] = _lwrist;
+        _l[1] = _lfa;
+        _l[2] = _lua;
+
+        _theta[0] = -pronoSupEncoder / _robot->joints.wrist_pronation->r_incs_per_deg();
+        _theta[1] = elbowEncoder / _robot->joints.elbow_flexion->r_incs_per_deg();
+        if (_cnt % 50 == 0)
+            debug() << "_theta(deg): " << _theta[0] << ", " << _theta[1] << "\r\n";
+        // in radians:
+        _theta[0] = _theta[0] * M_PI / 180;
+        _theta[1] = _theta[1] * M_PI / 180;
+
+        _lambda[0] = _lambdaWPS;
+        _lambda[1] = _lambdaE;
+    }
+
+    // create and set posA and posHip to zero. posHip is useless but need to be defined for some _lawJ functions
+    Eigen::Vector3d posA = Eigen::Vector3d::Zero(), posHip = Eigen::Vector3d::Zero();
+
+    /// IMU - Initialization data
+    _qHip.w() = 0.;
+    _qHip.x() = 0.;
+    _qHip.y() = 0.;
+    _qHip.z() = 0.;
+    _qHand.w() = 0.;
+    _qHand.x() = 0.;
+    _qHand.y() = 0.;
+    _qHand.z() = 0.;
+    _qTrunk.w() = 0.;
+    _qTrunk.x() = 0.;
+    _qTrunk.y() = 0.;
+    _qTrunk.z() = 0.;
+
+    double qWhite[4], qRed[4], qYellow[4];
+    if (_robot->sensors.white_imu) {
+        _robot->sensors.white_imu->get_quat(qWhite);
+        _qHand.w() = qWhite[0];
+        _qHand.x() = qWhite[1];
+        _qHand.y() = qWhite[2];
+        _qHand.z() = qWhite[3];
+    }
+    if (_robot->sensors.red_imu) {
+        _robot->sensors.red_imu->get_quat(qRed);
+        _qHip.w() = qRed[0];
+        _qHip.x() = qRed[1];
+        _qHip.y() = qRed[2];
+        _qHip.z() = qRed[3];
+        //        debug() << "qRed: " << _qHip.w() << "; " << _qHip.x() << "; " << _qHip.y() << "; " << _qHip.z();
+    }
+    if (_robot->sensors.yellow_imu) {
+        _robot->sensors.yellow_imu->get_quat(qYellow);
+        _qTrunk.w() = qYellow[0];
+        _qTrunk.x() = qYellow[1];
+        _qTrunk.y() = qYellow[2];
+        _qTrunk.z() = qYellow[3];
+        //        debug() << "qyellow: " << qYellow[0] << "; " << qYellow[1] << "; " << qYellow[2] << "; " << qYellow[3];
+    }
+
+    /// PIN PUSH-BUTTONS CONTROL
+    int pin_down_value = digitalRead(_pin_down);
+    int pin_up_value = digitalRead(_pin_up);
+
+    /// CONTROL LOOP
+    if (_cnt == 0) {
+        initializationLaw(_qHip, 1 / period());
+    } else if (_cnt <= _init_cnt) {
+        initialPositionsLaw(_qHand, _qHip, _qTrunk, _theta, _lt, _lsh, _l, _nbDOF, _cnt, _init_cnt);
+    } else {
+        controlLaw(_qHand, _qHip, _qTrunk, _theta, _lt, _lsh, _l, _nbDOF, _k, _lambda, _threshold, _cnt, _init_cnt);
+
+        Eigen::Matrix<double, nbLinks, 1, Eigen::DontAlign> _thetaDot_toSend = _lawJ.returnthetaDot_deg();
+
+        if (_robot->joints.wrist_flexion) {
+            _robot->joints.wrist_flexion->set_velocity_safe(-_thetaDot_toSend[0]);
+            _robot->joints.wrist_pronation->set_velocity_safe(-_thetaDot_toSend[1]);
+            _robot->joints.elbow_flexion->set_velocity_safe(-_thetaDot_toSend[2]);
+            if (_cnt % 50 == 0) {
+                debug() << "wrist flex vel :" << _thetaDot_toSend[0] << "\n";
+                debug() << "pronosup vel :" << _thetaDot_toSend[1] << "\n";
+                debug() << "elbow flex vel :" << _thetaDot_toSend[2] << "\n";
+            }
+        } else {
+            _robot->joints.wrist_pronation->set_velocity_safe(-_thetaDot_toSend[0]);
+            _robot->joints.elbow_flexion->set_velocity_safe(_thetaDot_toSend[1]);
+            if (_cnt % 50 == 0) {
+                debug() << "pronosup vel :" << _thetaDot_toSend[0] << "\n";
+                debug() << "elbow flex vel :" << _thetaDot_toSend[1] << "\n";
+            }
+        }
+    }
+
+    _lawJ.writeDebugData(debugData, _theta);
+    /// WRITE DATA
+    _file << _nbDOF << timeWithDelta << ' ' << pin_down_value << ' ' << pin_up_value << ' ' << _lt << ' ' << _lsh << ' ' << _lua << ' ' << _lfa << ' ' << _lwrist;
+    _file << ' ' << _lambda[0] << ' ' << _lambda[1] << ' ' << _lambda[2] << ' ' << _threshold[0] << ' ' << _threshold[1] << ' ' << _threshold[2];
+    _file << ' ' << qWhite[0] << ' ' << qWhite[1] << ' ' << qWhite[2] << ' ' << qWhite[3] << ' ' << qRed[0] << ' ' << qRed[1] << ' ' << qRed[2] << ' ' << qRed[3];
+    _file << ' ' << qYellow[0] << ' ' << qYellow[1] << ' ' << qYellow[2] << ' ' << qYellow[3];
+    for (int i = 0; i < 40; i++) {
+        _file << ' ' << debugData[i];
+    }
+    _file << ' ' << pronoSupEncoder << ' ' << wristFlexEncoder << ' ' << elbowEncoder;
+
+    _file << std::endl;
+
+    ++_cnt;
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count() << "ms" << std::endl;
+}
+
+void ControlIMU::cleanup()
+{
+    _robot->joints.wrist_pronation->forward(0);
+    if (_robot->joints.wrist_flexion)
+        _robot->joints.wrist_flexion->forward(0);
+    //    _robot->joints.elbow_flexion->move_to(0, 20);
+    if (_robot->joints.hand)
+        _robot->joints.hand->release_ownership();
+    _file.close();
+}
+
+void ControlIMU::initializationLaw(Eigen::Quaterniond qHi, double p)
+{
+}
+
+void ControlIMU::initialPositionsLaw(Eigen::Quaterniond qHa, Eigen::Quaterniond qHi, Eigen::Quaterniond qT, double theta[], int lt, int lsh, int l[], int nbDOF, int cnt, int init_cnt)
+{
+}
+
+void ControlIMU::controlLaw(Eigen::Quaterniond qHa, Eigen::Quaterniond qHi, Eigen::Quaterniond qT, double theta[], int lt, int lsh, int l[], int nbDOF, int k, int lambda[], double threshold[], int cnt, int init_cnt)
+{
 }
--- src/control/controlimu.h
+++ src/control/controlimu.h
@@ -1,11 +1,65 @@
 #ifndef CONTROLIMU_H
 #define CONTROLIMU_H
 
+#include "algo/lawjacobian.h"
+#include "sam/sam.h"
+#include "utils/named_object.h"
+#include "utils/socket.h"
+#include "utils/threaded_loop.h"
+#include <fstream>
 
-class ControlIMU
-{
+class ControlIMU : public ThreadedLoop {
 public:
-    ControlIMU();
+    explicit ControlIMU(std::string name, std::string filename, std::shared_ptr<SAM::Components> robot);
+    ~ControlIMU() override;
+
+    std::shared_ptr<SAM::Components> _robot;
+    std::ofstream _file;
+    bool _need_to_write_header;
+    std::string _filename;
+    clock::time_point _start_time;
+
+    LawJacobian _lawJ;
+
+    void tare_IMU();
+    void analog_IMU();
+    void displayPin();
+    void calibrations();
+
+    bool setup() override;
+    void loop(double dt, clock::time_point time) override;
+    void cleanup() override;
+
+protected:
+    virtual void initializationLaw(Eigen::Quaterniond qHi, double p);
+    virtual void initialPositionsLaw(Eigen::Quaterniond qHa, Eigen::Quaterniond qHi, Eigen::Quaterniond qT, double theta[], int lt, int lsh, int l[], int nbDOF, int cnt, int init_cnt);
+    virtual void controlLaw(Eigen::Quaterniond qHa, Eigen::Quaterniond qHi, Eigen::Quaterniond qT, double theta[], int lt, int lsh, int l[], int nbDOF, int k, int lambda[], double threshold[], int cnt, int init_cnt);
+
+private:
+    Param<int> _k; // damping parameter for inverse kinematics
+    Param<int> _lt; // length of the trunk
+    Param<int> _lsh; // length between neck and shoulder
+    Param<int> _lua; // upper-arm length
+    Param<int> _lfa; // forearm length
+    Param<int> _lwrist; // length between flexion and pronosupination joints
+    Param<int> _lambdaE; // gain for elbow flexion
+    Param<int> _lambdaWF; // gain for wrist flexion
+    Param<int> _lambdaWPS; // gain for wrist pronosupination
+    Param<double> _thresholdE; // threshold for elbow flexion
+    Param<double> _thresholdWF; // threshold for wrist flexion
+    Param<double> _thresholdWPS; // thresold for wrist pronosupination
+
+    int _nbDOF;
+    int _init_cnt = 10;
+    int _cnt;
+    int _lambda[nbLinks];
+    int _pin_up;
+    int _pin_down;
+    double _theta[nbLinks];
+    double _threshold[nbLinks];
+    int _l[nbLinks];
+
+    Eigen::Quaterniond _qHip, _qTrunk, _qHand;
 };
 
-#endif // CONTROLIMU_H
+#endif // CONTROL_IMU_H
