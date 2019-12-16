9aed83b5c6de767b28ac2fcb806e6b48a0ae3b64
Charlotte <marchand@isir.upmc.fr>
2019-12-16 12:17:27 +0100

    Add maximum security velocity for cybathlon elbow

--- src/components/internal/actuators/elbow_cybathlon.cpp
+++ src/components/internal/actuators/elbow_cybathlon.cpp
@@ -8,7 +8,9 @@ ElbowCybathlon::ElbowCybathlon()
     _menu->set_description("Elbow Cybathlon - " + read_firmware_version());
     _menu->set_code("elbow");
 
-    set_params_limits(0., 100.);
+    _max_velocity = 15;
+
+    set_params_limits(0, 100.);
     set_params_technical(4065, 100);
     set_params_velocity(0.06070f, 0.00427f, 0, 898312);
     set_params_position(21.1098f, 0., 0., 0., 0., -180., 180.);
@@ -16,7 +18,25 @@ ElbowCybathlon::ElbowCybathlon()
 
 void ElbowCybathlon::calibrate()
 {
-    Actuator::calibrate(-5, 2, 0.5, false);
-    move_to(0, 30, true);
+    Actuator::calibrate(-5, -15, 0.5, false);
+    move_to(0, 15, true);
 }
 
+void ElbowCybathlon::move_to(double deg, double speed, bool block)
+{
+    if (speed > _max_velocity)
+    {
+        speed = _max_velocity;
+    } else if (speed < -_max_velocity)
+    {
+        speed = -_max_velocity;
+    }
+
+    Actuator::move_to(deg,speed,block);
+
+}
+
+void ElbowCybathlon::set_max_velocity(double deg_s)
+{
+    _max_velocity = deg_s;
+}
--- src/components/internal/actuators/elbow_cybathlon.h
+++ src/components/internal/actuators/elbow_cybathlon.h
@@ -8,6 +8,12 @@ class ElbowCybathlon : public Actuator
 public:
     ElbowCybathlon();
     void calibrate();
+    void move_to(double deg, double speed, bool block = false);
+    void set_max_velocity(double speed);
+
+private:
+    double _max_velocity;
+
 };
 
 #endif // ELBOWCYBATHLON_H
