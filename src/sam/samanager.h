fca0a1c62d499ea531b1854eab4388b88d2537c7
Mathilde <mathilde.lestoille@gmail.com>
2020-01-06 15:37:26 +0100

    comment and uncomment

--- src/sam/samanager.h
+++ src/sam/samanager.h
@@ -10,10 +10,10 @@
 #include "control/jfimu_v3.h"
 #include "control/jfimu_v4.h"
 #include "control/matlab_receiver.h"
+#include "control/read_adc.h"
 #include "control/recorddata.h"
 #include "control/remote_computer_control.h"
 #include "control/voluntary_control.h"
-#include "control/read_adc.h"
 #include "sam/sam.h"
 #include "sam/system_monitor.h"
 #include "ui/menu/menu_console.h"
@@ -39,18 +39,19 @@ private:
     std::mutex _cv_mutex;
 
     std::unique_ptr<VoluntaryControl> _vc;
-    std::unique_ptr<JacobianFormulationOpti> _jfOpti;
-    std::unique_ptr<JacobianFormulationIMU> _jfIMU1;
-    std::unique_ptr<JFIMU_v3> _jfIMU3;
-    std::unique_ptr<JFIMU_v4> _jfIMU4;
-    std::unique_ptr<RecordData> _recordData;
+    //    std::unique_ptr<JacobianFormulationOpti> _jfOpti;
+    //    std::unique_ptr<JacobianFormulationIMU> _jfIMU1;
+    //    std::unique_ptr<JFIMU_v3> _jfIMU3;
+    //    std::unique_ptr<JFIMU_v4> _jfIMU4;
+    //    std::unique_ptr<RecordData> _recordData;
     std::unique_ptr<CompensationOptitrack> _opti;
     std::unique_ptr<RemoteComputerControl> _rm;
     std::unique_ptr<MatlabReceiver> _mr;
-    std::unique_ptr<Demo> _demo;
-    std::unique_ptr<DemoIMU> _demoimu;
+    //    std::unique_ptr<Demo> _demo;
+    //    std::unique_ptr<DemoIMU> _demoimu;
     std::unique_ptr<SystemMonitor> _sm;
     std::unique_ptr<ReadADC> _adc;
+    std::unique_ptr<CompensationIMU> _imu;
 
     std::unique_ptr<MenuBackend> _main_menu;
     std::unique_ptr<MenuMQTT> _menu_mqtt_binding;
