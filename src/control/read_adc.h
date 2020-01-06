70c917b3b6c1319ba9a9e7a6c0e1214759f585e2
Mathilde <mathilde.lestoille@gmail.com>
2020-01-06 15:36:51 +0100

    add tareIMU function and unique elbow calibration

--- src/control/read_adc.h
+++ src/control/read_adc.h
@@ -2,8 +2,9 @@
 #define READADC_H
 
 #include "sam/sam.h"
-#include "utils/threaded_loop.h"
 #include "utils/interfaces/mqtt_user.h"
+#include "utils/threaded_loop.h"
+#include <fstream>
 
 class ReadADC : public ThreadedLoop, public MqttUser {
 public:
@@ -13,12 +14,16 @@ public:
     bool setup() override;
     void loop(double dt, clock::time_point time) override;
     void cleanup() override;
+    void tareIMU();
+    clock::time_point _start_time;
 
 private:
     std::shared_ptr<SAM::Components> _robot;
-    static const uint16_t _n_electrodes=6;
+    static const uint16_t _n_electrodes = 6;
     int _th_low[_n_electrodes];
     int _th_high[_n_electrodes];
+
+    std::ofstream _file;
 };
 
 #endif // READADC_H
