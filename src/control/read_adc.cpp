c725769b6122e10b5e7dd8a975efdd969527f663
Mathilde <mathilde.lestoille@gmail.com>
2020-01-08 17:07:29 +0100

    change preset period

--- src/control/read_adc.cpp
+++ src/control/read_adc.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 ReadADC::ReadADC(std::shared_ptr<SAM::Components> robot)
-    : ThreadedLoop("Read ADC", .04)
+    : ThreadedLoop("Read ADC", .02)
     , _robot(robot)
 {
     _menu->set_description("Read ADC");
@@ -58,10 +58,10 @@ bool ReadADC::setup()
     }
     _robot->joints.wrist_pronation->calibrate();
 
-    //_mqtt.publish("sam/emg/th/1", std::to_string(_th_low[0]));
-    //_mqtt.publish("sam/emg/th/2", std::to_string(_th_high[0]));
-    //_mqtt.subscribe("sam/emg/th/1", Mosquittopp::Client::QoS1)->add_callback(this, [this](Mosquittopp::Message msg) {this->_th_low[0] = std::stoi(msg.payload()); });
-    //_mqtt.subscribe("sam/emg/th/2", Mosquittopp::Client::QoS1)->add_callback(this, [this](Mosquittopp::Message msg) {this->_th_high[0] = std::stoi(msg.payload()); });
+    _mqtt.publish("sam/emg/th/1", std::to_string(_th_low[0]));
+    _mqtt.publish("sam/emg/th/2", std::to_string(_th_high[0]));
+    _mqtt.subscribe("sam/emg/th/1", Mosquittopp::Client::QoS1)->add_callback(this, [this](Mosquittopp::Message msg) { this->_th_low[0] = std::stoi(msg.payload()); });
+    _mqtt.subscribe("sam/emg/th/2", Mosquittopp::Client::QoS1)->add_callback(this, [this](Mosquittopp::Message msg) { this->_th_high[0] = std::stoi(msg.payload()); });
 
     //    _robot->user_feedback.leds->set(LedStrip::white, 10);
 
@@ -106,35 +106,35 @@ void ReadADC::loop(double, clock::time_point time)
 
     std::string payload;
 
-    electrodes[0] = _robot->sensors.adc0->readADC_SingleEnded(2);
-    electrodes[1] = _robot->sensors.adc0->readADC_SingleEnded(3);
-    electrodes[2] = _robot->sensors.adc2->readADC_SingleEnded(2);
-    electrodes[3] = _robot->sensors.adc2->readADC_SingleEnded(0);
-    electrodes[4] = _robot->sensors.adc3->readADC_SingleEnded(2);
-    electrodes[5] = _robot->sensors.adc3->readADC_SingleEnded(0);
+    //    electrodes[0] = _robot->sensors.adc0->readADC_SingleEnded(2);
+    //    electrodes[1] = _robot->sensors.adc0->readADC_SingleEnded(3);
+    //    electrodes[2] = _robot->sensors.adc2->readADC_SingleEnded(2);
+    //    electrodes[3] = _robot->sensors.adc2->readADC_SingleEnded(0);
+    //    electrodes[4] = _robot->sensors.adc3->readADC_SingleEnded(2);
+    //    electrodes[5] = _robot->sensors.adc3->readADC_SingleEnded(0);
 
-    //    if (electrodes[2] > _th_high[2] && electrodes[4] < _th_low[4] && electrodes[3] < _th_high[3]) {
-    //        _robot->joints.elbow_flexion->set_velocity_safe(25);
-    //    } else if (electrodes[4] > _th_high[4] && electrodes[2] < _th_low[2] && electrodes[3] < _th_high[3]) {
-    //        _robot->joints.elbow_flexion->set_velocity_safe(-25);
-    //    } else {
-    //        _robot->joints.elbow_flexion->set_velocity_safe(0);
-    //    }
+    if (electrodes[2] > _th_high[2] && electrodes[4] < _th_low[4] && electrodes[3] < _th_high[3]) {
+        _robot->joints.elbow_flexion->set_velocity_safe(25);
+    } else if (electrodes[4] > _th_high[4] && electrodes[2] < _th_low[2] && electrodes[3] < _th_high[3]) {
+        _robot->joints.elbow_flexion->set_velocity_safe(-25);
+    } else {
+        _robot->joints.elbow_flexion->set_velocity_safe(0);
+    }
 
-    //    if (electrodes[3] > _th_high[3] && electrodes[5] < _th_low[5]) {
-    //        _robot->joints.wrist_pronation->set_velocity_safe(-40);
-    //    } else if (electrodes[5] > _th_high[5] && electrodes[3] < _th_low[3]) {
-    //        _robot->joints.wrist_pronation->set_velocity_safe(40);
-    //    } else {
-    //        _robot->joints.wrist_pronation->set_velocity_safe(0);
-    //    }
+    if (electrodes[3] > _th_high[3] && electrodes[5] < _th_low[5]) {
+        _robot->joints.wrist_pronation->set_velocity_safe(-40);
+    } else if (electrodes[5] > _th_high[5] && electrodes[3] < _th_low[3]) {
+        _robot->joints.wrist_pronation->set_velocity_safe(40);
+    } else {
+        _robot->joints.wrist_pronation->set_velocity_safe(0);
+    }
 
-    //    if (electrodes[0] > _th_high[0] && electrodes[1] < _th_low[1]) {
-    //        // _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,1);
-    //    } else if (electrodes[1] > _th_high[1] && electrodes[0] < _th_low[0]) {
-    //        // _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,1);
-    //    } else {
-    //    }
+    if (electrodes[0] > _th_high[0] && electrodes[1] < _th_low[1]) {
+        // _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,1);
+    } else if (electrodes[1] > _th_high[1] && electrodes[0] < _th_low[0]) {
+        // _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,1);
+    } else {
+    }
 
     for (uint16_t i = 0; i < _n_electrodes; i++) {
         colors[i] = LedStrip::white;
@@ -144,22 +144,22 @@ void ReadADC::loop(double, clock::time_point time)
         } else if (electrodes[i] > _th_low[i]) {
             colors[i] = LedStrip::green;
         }
-        //        _mqtt.publish("sam/emg/time/" + std::to_string(i), std::to_string(electrodes[i]));
-        //        std::cout << electrodes[i] << "\t";
+        _mqtt.publish("sam/emg/time/" + std::to_string(i), std::to_string(electrodes[i]));
+        std::cout << electrodes[i] << "\t";
     }
 
-    //    std::cout << std::endl;
+    std::cout << std::endl;
     //    _robot->user_feedback.leds->set(colors);
 
-    //    _file << timeWithDelta << ' ' << electrodes[0] << ' ' << electrodes[1] << ' ' << electrodes[2] << ' ' << electrodes[3] << ' ' << electrodes[4] << ' ' << electrodes[5];
-    //    _file << ' ' << qBras[0] << ' ' << qBras[1] << ' ' << qBras[2] << ' ' << qBras[3] << ' ' << qTronc[0] << ' ' << qTronc[1] << ' ' << qTronc[2] << ' ' << qTronc[3];
-    //    _file << ' ' << beta;
+    _file << timeWithDelta << ' ' << electrodes[0] << ' ' << electrodes[1] << ' ' << electrodes[2] << ' ' << electrodes[3] << ' ' << electrodes[4] << ' ' << electrodes[5];
+    _file << ' ' << qBras[0] << ' ' << qBras[1] << ' ' << qBras[2] << ' ' << qBras[3] << ' ' << qTronc[0] << ' ' << qTronc[1] << ' ' << qTronc[2] << ' ' << qTronc[3];
+    _file << ' ' << beta;
     //    for (unsigned int i = 0; i < data.nRigidBodies; i++) {
     //        _file << ' ' << data.rigidBodies[i].ID << ' ' << data.rigidBodies[i].bTrackingValid << ' ' << data.rigidBodies[i].fError;
     //        _file << ' ' << data.rigidBodies[i].qw << ' ' << data.rigidBodies[i].qx << ' ' << data.rigidBodies[i].qy << ' ' << data.rigidBodies[i].qz;
     //        _file << ' ' << data.rigidBodies[i].x << ' ' << data.rigidBodies[i].y << ' ' << data.rigidBodies[i].z;
     //    }
-    //    _file << std::endl;
+    _file << std::endl;
 
     //    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count() << "ms" << std::endl;
 }
