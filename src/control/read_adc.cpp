6493fbf14e6c580551f32e6df50a17d13079c38c
Mathilde <mathilde.lestoille@gmail.com>
2020-01-24 14:33:10 +0100

    Auto stash before cherrypick of "Classifier for quantum hand changing mode"

--- src/control/read_adc.cpp
+++ src/control/read_adc.cpp
@@ -2,6 +2,7 @@
 #include "ui/visual/ledstrip.h"
 #include <filesystem>
 #include <iostream>
+#include "algo/myocontrol.h"
 
 ReadADC::ReadADC(std::shared_ptr<SAM::Components> robot)
     : ThreadedLoop("Read ADC", .025)
@@ -15,7 +16,7 @@ ReadADC::ReadADC(std::shared_ptr<SAM::Components> robot)
     _th_low[0] = 3000;
     _th_low[1] = 1500;
     _th_low[2] = 2000;
-    _th_low[3] = 3000;
+    _th_low[3] = 2000;
     _th_low[4] = 2000;
     _th_low[5] = 1000;
 
@@ -24,7 +25,7 @@ ReadADC::ReadADC(std::shared_ptr<SAM::Components> robot)
     _th_high[2] = 3000;
     _th_high[3] = 3000;
     _th_high[4] = 2000;
-    _th_high[5] = 5000;
+    _th_high[5] = 4000;
 
     for (uint16_t i = 0; i < _n_electrodes; i++) {
         _electrodes[i] = 0;
@@ -104,8 +105,8 @@ void ReadADC::readAllADC() //Optimized function to read all 6 electrodes
         }
     }
 
-    clock::time_point time2 = clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count() << std::endl;
+    //clock::time_point time2 = clock::now();
+    //std::cout << std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count() << " us" << std::endl;
 }
 
 bool ReadADC::setup()
@@ -176,56 +177,100 @@ void ReadADC::loop(double, clock::time_point time)
 
     double beta = _robot->joints.elbow_flexion->pos() * M_PI / 180.;
 
-    std::vector<LedStrip::color> colors(11, LedStrip::white);
+    static std::unique_ptr<MyoControl::Classifier> handcontrol;
 
-    std::string payload;
+    static const unsigned int counts_after_mode_change = 15;
+    static const unsigned int counts_btn = 2;
+    static const unsigned int counts_before_bubble = 2;
+    static const unsigned int counts_after_bubble = 2;
 
-    /// Old reading
-    //    _electrodes[0] = _robot->sensors.adc0->readADC_SingleEnded(2);
-    //    _electrodes[1] = _robot->sensors.adc0->readADC_SingleEnded(3);
-    //    _electrodes[2] = _robot->sensors.adc2->readADC_SingleEnded(2);
-    //    _electrodes[3] = _robot->sensors.adc2->readADC_SingleEnded(0);
-    //    _electrodes[4] = _robot->sensors.adc3->readADC_SingleEnded(2);
-    //    _electrodes[5] = _robot->sensors.adc3->readADC_SingleEnded(0);
+    static const MyoControl::EMGThresholds thresholds(5000, 1500, 0, 5000, 1500, 0);
+
+    auto robot = _robot;
+
+    MyoControl::Action co_contraction("Co contraction",
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::CO_CONTRACTION); });
+    MyoControl::Action double_contraction("Double contraction",
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::DOUBLE_CONTRACTION); });
+    MyoControl::Action triple_contraction("Triple contraction",
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,1,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,4); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION,2,2); },
+        [robot]() { robot->joints.hand_quantum->makeContraction(QuantumHand::TRIPLE_CONTRACTION); });
+
+    std::vector<MyoControl::Action> s1 {co_contraction, double_contraction, triple_contraction};
+
+    static bool first = true;
+    if (first) {
+        handcontrol = std::make_unique<MyoControl::QuantumClassifier>(s1, thresholds, counts_after_mode_change, counts_btn, counts_before_bubble, counts_after_bubble);
+        first = false;
+    }
+
+    static std::vector<LedStrip::color> colors(11, LedStrip::white);
+
+    static std::string payload;
+
+    /* _electrodes[0] = _robot->sensors.adc0->readADC_SingleEnded(2);
+    _electrodes[1] = _robot->sensors.adc0->readADC_SingleEnded(3);
+    _electrodes[2] = _robot->sensors.adc2->readADC_SingleEnded(2);
+    _electrodes[3] = _robot->sensors.adc2->readADC_SingleEnded(0);
+    _electrodes[4] = _robot->sensors.adc3->readADC_SingleEnded(2);
+    _electrodes[5] = _robot->sensors.adc3->readADC_SingleEnded(0); */
 
     readAllADC();
 
-    if (_electrodes[2] > _th_high[2] && _electrodes[4] < _th_low[4] && _electrodes[3] < _th_high[3]) {
-        _robot->joints.elbow_flexion->set_velocity_safe(25);
-    } else if (_electrodes[4] > _th_high[4] && _electrodes[2] < _th_low[2] && _electrodes[3] < _th_high[3]) {
-        _robot->joints.elbow_flexion->set_velocity_safe(-25);
+    //EMG3 and EMG5 = elbow
+    if (_electrodes[2]>_th_high[2] && _electrodes[4]<_th_low[4] && _electrodes[3]<(_electrodes[2]-1500)) { //EMG3 activation
+        _robot->joints.elbow_flexion->set_velocity_safe(25); //Elbow flexion
+        colors[2] = LedStrip::red_bright;
+    } else if (_electrodes[4]>_th_high[4] && _electrodes[2]<_th_low[2] && _electrodes[3]<_th_low[3] && _electrodes[5]<_th_low[3]) { //EMG5 activation
+        _robot->joints.elbow_flexion->set_velocity_safe(-25); //Elbow extension
+        colors[4] = LedStrip::red_bright;
     } else {
         _robot->joints.elbow_flexion->set_velocity_safe(0);
     }
 
-    if (_electrodes[3] > _th_high[3] && _electrodes[5] < _th_low[5]) {
+    //EMG4 and EMG6 = wrist rotator
+    if (_electrodes[3]>_th_high[3] && _electrodes[5]<(_electrodes[3]-500) && _electrodes[4]<(_electrodes[3]-500) && _electrodes[2]<_electrodes[3]) { //EMG4 activation
         _robot->joints.wrist_pronation->set_velocity_safe(-40);
-    } else if (_electrodes[5] > _th_high[5] && _electrodes[3] < _th_low[3]) {
+        colors[3] = LedStrip::red_bright;
+    } else if (_electrodes[5]>_th_high[5] && _electrodes[3]<_th_high[5] && _electrodes[4]<_th_high[5]) { //EMG6 activation
         _robot->joints.wrist_pronation->set_velocity_safe(40);
+        colors[5] = LedStrip::red_bright;
     } else {
         _robot->joints.wrist_pronation->set_velocity_safe(0);
     }
 
-    if (_electrodes[0] > _th_high[0] && _electrodes[1] < _th_low[1]) {
-        _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION, 1, 1);
-    } else if (_electrodes[1] > _th_high[1] && _electrodes[0] < _th_low[0]) {
-        _robot->joints.hand_quantum->makeContraction(QuantumHand::SHORT_CONTRACTION, 2, 1);
+    //EMG1 and EMG2 = hand
+    static bool btn = 0;
+    if (!_robot->btn1) {
+        btn = 1;
     } else {
+        btn = 0;
     }
+    handcontrol->process(_electrodes[0], _electrodes[1], btn);
 
+    //Publish EMG values with MQTT
     for (uint16_t i = 0; i < _n_electrodes; i++) {
-        colors[i] = LedStrip::white;
-
-        if (_electrodes[i] > _th_high[i]) {
-            colors[i] = LedStrip::red_bright;
-        } else if (_electrodes[i] > _th_low[i]) {
-            colors[i] = LedStrip::green;
-        }
-        //        _mqtt.publish("sam/emg/time/" + std::to_string(i), std::to_string(_electrodes[i]));
+        _mqtt.publish("sam/emg/time/" + std::to_string(i), std::to_string(_electrodes[i]));
         std::cout << _electrodes[i] << "\t";
     }
-    std::cout << std::endl;
-    //    _robot->user_feedback.leds->set(colors);
+    std::cout << btn << std::endl;
+
+    //LED feedback
+    _robot->user_feedback.leds->set(colors);
+    for (uint16_t i = 0; i < _n_electrodes; i++) {
+        colors[i] = LedStrip::white;
+    }
 
     std::cout << std::endl;
     //    _robot->user_feedback.leds->set(colors);
@@ -247,6 +292,6 @@ void ReadADC::cleanup()
 {
     _robot->joints.wrist_pronation->set_velocity_safe(0);
     _robot->joints.elbow_flexion->set_velocity_safe(0);
-    _robot->user_feedback.leds->set(LedStrip::none, 10);
+    _robot->user_feedback.leds->set(LedStrip::white, 11);
     _file.close();
 }
