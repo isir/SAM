c310403477a32b22614da21a0d5723f70fb3e0e5
Charlotte <marchand@isir.upmc.fr>
2020-10-06 14:05:12 +0200

    Unimportant changes in menus

--- src/sam/samanager.cpp
+++ src/sam/samanager.cpp
@@ -79,12 +79,12 @@ void SAManager::fill_menus()
     _main_menu->add_submenu_from_user(_opti);
     _main_menu->add_submenu_from_user(_demo);
     _main_menu->add_submenu_from_user(_testimu);
-//    _main_menu->add_submenu_from_user(_demoimu);
+    _main_menu->add_submenu_from_user(_demoimu);
     _main_menu->add_submenu_from_user(_pb);
     _main_menu->add_submenu_from_user(_myo2);
     _main_menu->add_submenu_from_user(_jfOpti);
-//    _main_menu->add_submenu_from_user(_jfIMU4);
-//    _main_menu->add_submenu_from_user(_recordData);
+    _main_menu->add_submenu_from_user(_jfIMU4);
+    _main_menu->add_submenu_from_user(_recordData);
 
     _main_menu->activate();
 }
@@ -123,10 +123,10 @@ void SAManager::instantiate_controllers()
         _demo = std::make_unique<Demo>(_robot);
     } catch (std::exception&) {
     }
-//    try {
-//        _demoimu = std::make_unique<DemoIMU>(_robot);
-//    } catch (std::exception&) {
-//    }
+    try {
+        _demoimu = std::make_unique<DemoIMU>(_robot);
+    } catch (std::exception&) {
+    }
     try {
         _adc = std::make_unique<ReadADC>(_robot);
     } catch (std::exception&) {
@@ -152,14 +152,14 @@ void SAManager::instantiate_controllers()
     //        _jfIMU3 = std::make_unique<JFIMU_v3>(_robot);
     //    } catch (std::exception&) {
     //    }
-    //    try {
-    //        _jfIMU4 = std::make_unique<JFIMU_v4>(_robot);
-    //    } catch (std::exception&) {
-    //    }
-    //    try {
-    //        _recordData = std::make_unique<RecordData>(_robot);
-    //    } catch (std::exception&) {
-    //    }
+        try {
+            _jfIMU4 = std::make_unique<JFIMU_v4>(_robot);
+        } catch (std::exception&) {
+        }
+        try {
+            _recordData = std::make_unique<RecordData>(_robot);
+        } catch (std::exception&) {
+        }
 }
 
 void SAManager::autostart_demo()
