b21d0e17f240c6a4540ae853bb388098382f68ec
Charlotte <marchand@isir.upmc.fr>
2020-10-06 14:01:12 +0200

    Change elbows upper limits

--- src/components/internal/actuators/custom_elbow.cpp
+++ src/components/internal/actuators/custom_elbow.cpp
@@ -8,7 +8,7 @@ CustomElbow::CustomElbow()
     _menu->set_description("Custom Elbow - " + read_firmware_version());
     _menu->set_code("elbow");
 
-    set_params_limits(-100., 0.);
+    set_params_limits(-115., 0.);
     set_params_technical(4065, 100);
     set_params_velocity(0.199f, 0.014f, 0, 268700);
     set_params_position(19.3f, 0., 0., 0., 0., -180., 180.);
