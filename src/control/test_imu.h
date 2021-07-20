995b9f2eeb54132cea1d63e6f66297def5dcdc5a
Charlotte <marchand@isir.upmc.fr>
2021-07-20 10:02:56 +0200

    Test for RS232

--- src/control/test_imu.h
+++ src/control/test_imu.h
@@ -1,12 +1,12 @@
 #ifndef TESTIMU_H
 #define TESTIMU_H
 
-#include "algo/lawjacobian.h"
 #include "sam/sam.h"
 #include "utils/socket.h"
 #include "utils/interfaces/mqtt_user.h"
 #include "utils/threaded_loop.h"
 #include <fstream>
+#include "utils/serial_port.h"
 
 class TestIMU : public ThreadedLoop, public MqttUser  {
 public:
@@ -14,17 +14,15 @@ public:
     ~TestIMU() override;
 
 private:
-    void tare_IMU();
     bool setup() override;
     void loop(double dt, clock::time_point time) override;
     void cleanup() override;
 
     std::shared_ptr<SAM::Components> _robot;
-    Socket _receiver;
-    std::ofstream _file;
-    int _cnt;
-    clock::time_point _start_time;
 
+    void send();
+
+    SerialPort _serial_port;
 };
 
 #endif // TESTIMU_H
