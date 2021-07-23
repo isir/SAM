eb362183fbc253ef46d817a5e9400b54f7a984e1
Charlotte <marchand@isir.upmc.fr>
2021-07-23 13:30:06 +0200

    Socket send

--- src/utils/socket.cpp
+++ src/utils/socket.cpp
@@ -5,9 +5,11 @@
 #include <stdexcept>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <string>
 
 Socket::Socket()
     : _sock_fd(socket(AF_INET, SOCK_DGRAM, 0))
+    , _new_fd(0)
 {
     if (_sock_fd < 0) {
         throw std::runtime_error("Failed to create socket");
@@ -31,15 +33,35 @@ bool Socket::available()
     return recv(_sock_fd, _buf, _buf_sz, MSG_DONTWAIT | MSG_PEEK) > 0;
 }
 
-std::vector<std::byte> Socket::receive()
+std::vector<std::byte> Socket::receive() //simple receive function
 {
-    ssize_t n = recv(_sock_fd, _buf, _buf_sz, MSG_DONTWAIT);
+    ssize_t n = ::recv(_sock_fd, _buf, _buf_sz, MSG_DONTWAIT);
     if (n > 0) {
         return std::vector<std::byte>(_buf, _buf + n);
     } else {
         return std::vector<std::byte>();
     }
 }
+
+std::vector<std::byte> Socket::receive(struct sockaddr& addr) //receive message and get the IP address from the sender
+{
+    socklen_t fromlen;
+    //struct sockaddr addr;
+    fromlen = sizeof addr;
+    ssize_t n = ::recvfrom(_sock_fd, _buf, _buf_sz, MSG_DONTWAIT, &addr, &fromlen);
+    if (n > 0) {
+        return std::vector<std::byte>(_buf, _buf + n);
+    } else {
+        return std::vector<std::byte>();
+    }
+}
+
+bool Socket::send(const char* message, size_t message_length, const struct sockaddr dest_addr)
+{
+    ssize_t r = ::sendto(_sock_fd, message, message_length, MSG_CONFIRM, &dest_addr, sizeof dest_addr);
+    return r == 0;
+}
+
 
 bool Socket::close()
 {
--- src/utils/socket.h
+++ src/utils/socket.h
@@ -12,11 +12,15 @@ public:
 
     bool available();
     std::vector<std::byte> receive();
+    std::vector<std::byte> receive(struct sockaddr& addr);
+
+    bool send(const char* message, size_t message_length, const struct sockaddr dest_addr);
 
     bool close();
 
 private:
     int _sock_fd;
+    int _new_fd;
 
     static const std::size_t _buf_sz = 1024;
     std::byte _buf[_buf_sz];
